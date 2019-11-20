d0a7a95fe9845b119e63286965743aaba650f092
keralex <kerlis.aguado26@gmail.com>
2019-11-19 23:49:15 -0300

    nav 2

--- src/App.js
+++ src/App.js
@@ -1,37 +1,36 @@
 import React from 'react';
-import './App.css';
-import Nav from './components/nav-component/nav.component';
+import './App.scss';
 import Home from './components/home-component/home.component';
 import History from './components/history-component/history.component';
 
 
 function App() {
   return (
-  <div>
- <div className="navbar">
-    <ul>
-      <li>
-        <a href="#home">Home</a>
-      </li>
-      <li>
-        <a href="#About">About</a>
-      </li>
-      <li>
-        <a href="#">Projects</a>
-      </li>
-      <li>
-        <a href="#">Contact</a>
-      </li>
-    </ul>
-  </div>
     <div>
-    <Home id='home'/>
+      <div className="navbar">
+        <ul>
+          <li>
+            <a href="#home">Home</a>
+          </li>
+          <li>
+            <a href="#About">About</a>
+          </li>
+          <li>
+            <a href="#">Projects</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <Home id='home' />
+      </div>
+      <div id='About'>
+        <History />
+      </div>
+
     </div>
-    <div id='About'> 
-    <History/>
-    </div>
-    
-  </div>
   );
 }
 
