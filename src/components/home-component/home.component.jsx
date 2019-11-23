92a6d6e5eaa4eeef3ec9a64bc2da92fd872dde62
keralex <kerlis.aguado26@gmail.com>
2019-11-22 21:41:52 -0300

    responsive home

--- src/components/home-component/home.component.jsx
+++ src/components/home-component/home.component.jsx
@@ -1,9 +1,16 @@
-import React, { Component } from 'react';
-import './home.styles.scss';
-const Home = ()=>(
-    <div className='home'>
-        this is the home page
+import React, { Component } from "react";
+import "./home.styles.scss";
+
+const Home = () => (
+  <div className="home">
+    <h1 className='left-box red'>HELLO</h1>
+    <div className="right-box">
+      <h1 className='red'>I'M KERLIS</h1>
+      <h2>WEB DEVELOPER</h2>
+
+
     </div>
-) 
+  </div>
+);
 
 export default Home;
