4b18491a38ca0dafdc8331c5439feb3e0a4eeafb
keralex <kerlis.aguado26@gmail.com>
2019-11-27 12:25:56 -0300

    skills

--- src/components/skills-components/skillls.components.jsx
+++ src/components/skills-components/skillls.components.jsx
@@ -1,20 +1,59 @@
-import React from 'react';
-import './skills.styles.scss';
+import React from "react";
+import "./skills.styles.scss";
 const Skills = () => (
-<div className='skills'>
-<div class="flip-card">
-  <div class="flip-card-inner">
-    <div class="flip-card-front">
-        heloooo
+  <div>
+    <h1>MY SKILLS</h1>
+<div className="skills">
+    <div className="left-circle"> 
+      <div class="flip-card">
+        <div class="flip-card-inner">
+          <div class="flip-card-front"><h2>IT SKILLS</h2></div>
+          <div class="flip-card-back">
+           <ul>
+             <li>HTML</li>
+             <li>CSS</li>
+             <li>JAVASCRIPT</li>
+             <li>SASS</li>
+             <li>SQL</li>
+             <li>TYPESCRIPT</li>
+             <li>ANGULAR 2+</li>
+             <li>REACTJS</li>
+             <li>NOJEJS</li>
+             <li>REDUX</li>
+             <li>FIREBASE</li>
+             <li>BOOTSTRAP</li>
+             <li>JAVA</li>
+             <li>GITHUB</li>
+           </ul>
+          </div>
+        </div>
+      </div>
     </div>
-    <div class="flip-card-back">
-      <h1>John Doe</h1> 
-      <p>Architect & Engineer</p> 
-      <p>We love that guy</p>
+
+    <div className="right-circle">
+      <div class="flip-card">
+        <div class="flip-card-inner">
+          <div class="flip-card-front"><h2>DESIGNING SKILLS</h2></div>
+          <div class="flip-card-back">
+          <ul>
+             <li>ADOBE PHOTOSHOP</li>
+             <li>ADOBE ILLUSTRATOR</li>
+             <li>CANVA</li>
+             <li>LUNACY</li>
+             <li>WORD</li>
+             <li>EXCEL</li>
+             <li>POWER POINT</li>
+             <li>ONE NOTE</li>
+             <li>GOOGLE DOCS</li>
+             <li>GOOGLE SHEETS</li>
+           </ul>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
-</div>
-</div>
-)
+  </div>
+  
+);
 
 export default Skills;
