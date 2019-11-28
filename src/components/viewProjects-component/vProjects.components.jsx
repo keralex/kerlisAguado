20736044ddef30670fcc46418f404848e01a40d0
keralex <kerlis.aguado26@gmail.com>
2019-11-28 01:08:21 -0300

    add components

---
+++ src/components/viewProjects-component/vProjects.components.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './vProjects.styles.scss';
+export class vProjects.components extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default vProjects.components;
