e0cae37d67a2f59d193b32d9fc71caeb54038c6a
keralex <kerlis.aguado26@gmail.com>
2019-11-28 12:59:18 -0300

    grid projects

--- src/components/viewProjects-component/vProjects.components.jsx
+++ src/components/viewProjects-component/vProjects.components.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import "./vProjects.styles.scss";
-import Project from '../projects-component/projects.components';
-
+import Project from "../projects-component/projects.components";
 
 export class VProjects extends Component {
   constructor() {
@@ -9,35 +8,36 @@ export class VProjects extends Component {
     this.state = {
       projects: [
         {
-          title: 'example',
-          imageUrl: 'example',
+          title: "example",
+          imageUrl: "example",
           id: 1,
-          linkCode: 'https://github.com/keralex',
-          previewCode: 'none'
+          linkCode: "https://github.com/keralex",
+          previewCode: "none"
         },
         {
-            title: 'example',
-            imageUrl: 'example',
-            id: 2,
-            linkCode: 'https://github.com/keralex',
-            previewCode: 'none'
-          }
+          title: "example",
+          imageUrl: "example",
+          id: 2,
+          linkCode: "https://github.com/keralex",
+          previewCode: "none"
+        }
       ]
     };
   }
   render() {
-    return(
-        <div>
-          <div className='project'>{
-            this.state.projects.map(({id,...otherSectionProps})=>(
-              <Project key={id} {...otherSectionProps}/>
-            ))
-
-          }
-            </div>
-    </div>
+    return (
+      <div className="wrapper">
+           <div className='left'>
+          <h1>PROJECTS</h1>
+        </div>
+        <div className="projects">
+          {this.state.projects.map(({ id, ...otherSectionProps }) => (
+            <Project key={id} {...otherSectionProps} />
+          ))}
+        </div>
+     
+      </div>
     );
-   
   }
 }
 export default VProjects;
