908d9161964bbe361addbbbd485194ab252ac776
keralex <kerlis.aguado26@gmail.com>
2019-12-05 13:04:23 -0300

    add projects

--- src/components/viewProjects-component/vProjects.components.jsx
+++ src/components/viewProjects-component/vProjects.components.jsx
@@ -14,7 +14,8 @@ export class VProjects extends Component {
           imageUrl: "example",
           id: 1,
           linkCode: "https://github.com/keralex/dressIt",
-          previewCode: "https://github.com/keralex/dressIt"
+          previewCode: "https://github.com/keralex/dressIt",
+          year: "2019"
         },
         {
           title: "Background Generator",
@@ -23,7 +24,52 @@ export class VProjects extends Component {
           imageUrl: "example",
           id: 2,
           linkCode: "https://github.com/keralex/Background-Generator",
-          previewCode: "https://keralex.github.io/Background-Generator/"
+          previewCode: "https://keralex.github.io/Background-Generator/",
+          year: "2019"
+
+        }, {
+          title: "APP TO-DO",
+          description:"Task list app",
+          lenguajes:"React JS",
+          imageUrl: "example",
+          id: 3,
+          linkCode: "https://github.com/keralex/ToDo-React",
+          previewCode: "https://github.com/keralex/ToDo-React",
+          year: "2019"
+
+        },
+        {
+          title: "pokedex",
+          description:"POKEMON SEARCH APP",
+          lenguajes:"ReactJs",
+          imageUrl: "example",
+          id: 4,
+          linkCode: "https://github.com/keralex/pokedex",
+          previewCode: "https://keralex.github.io/pokedex/",
+          year: "2019"
+
+        },
+        {
+          title: "VUELOS GUACAMAYA",
+          description:"administrative system simulation of an airline",
+          lenguajes:"Angular/NodeJs/mySQL",
+          imageUrl: "example",
+          id: 5,
+          linkCode: "https://github.com/keralex/Guacamaya",
+          previewCode: null,
+          year: "2019"
+
+        },
+        {
+          title: "uMakeIt",
+          description:"Delivery Sushi App",
+          lenguajes:"Angular 6",
+          imageUrl: "example",
+          id: 6,
+          linkCode: "https://github.com/keralex/umakeit/tree/master/proyecto",
+          previewCode: "https://umakeit-5d5ca.firebaseapp.com/login",
+          year: "2018"
+
         },
      
       ]
