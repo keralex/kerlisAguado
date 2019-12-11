7da6b4a27a63ee8cd64b8ea8b57ddd241599a8b0
keralex <kerlis.aguado26@gmail.com>
2019-12-11 17:45:19 -0300

    contact me

--- src/components/contact-component/contact.component.jsx
+++ src/components/contact-component/contact.component.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
+import './contact.styles.scss';
+import twitter from '../../assets/asset1.png';
 
 const Contact = () =>(
-    <div>
-        <h1>contact</h1>
+    <div className='wrapperbox'>
+        <div className='bigcircle'>
+            <div>
+            <h1>CONTACT ME</h1>
+            <p>Hi! if you want us to work toguether contact me!</p>
+            <p>I'm available for frelance projects and full-time jobs.</p>
+            <p>Kerlis.aguado26@gmail.com</p>
+            <p>+54 911 54635604</p>
+            <div className='icons'>
+                <img src={twitter} alt='twitter icon'/>
+            </div>
+            </div>
+        
+        </div>
     </div>
 )
 
