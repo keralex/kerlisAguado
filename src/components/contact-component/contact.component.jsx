27c9b89665ee82f475f0d7413a5353641f7ef0eb
keralex <kerlis.aguado26@gmail.com>
2019-12-11 23:30:53 -0300

    Contact

--- src/components/contact-component/contact.component.jsx
+++ src/components/contact-component/contact.component.jsx
@@ -1,23 +1,38 @@
-import React from 'react';
-import './contact.styles.scss';
-import twitter from '../../assets/asset1.png';
+import React from "react";
+import "./contact.styles.scss";
+import twitter from "../../assets/asset1.png";
+import linke from "../../assets/Asset3.png";
+import behance from "../../assets/Asset2.png";
+import gitlogo from "../../assets/github-logo.svg";
 
-const Contact = () =>(
-    <div className='wrapperbox'>
-        <div className='bigcircle'>
-            <div>
-            <h1>CONTACT ME</h1>
-            <p>Hi! if you want us to work toguether contact me!</p>
-            <p>I'm available for frelance projects and full-time jobs.</p>
-            <p>Kerlis.aguado26@gmail.com</p>
-            <p>+54 911 54635604</p>
-            <div className='icons'>
-                <img src={twitter} alt='twitter icon'/>
-            </div>
-            </div>
-        
+const Contact = () => (
+  <div className="wrapperbox">
+    <div className="bigcircle">
+      <div>
+        <h1>CONTACT ME</h1>
+        <p>Hi! if you want us to work toguether contact me!</p>
+        <p>I'm available for frelance projects and full-time jobs.</p>
+        <p>Kerlis.aguado26@gmail.com</p>
+        <p>+54 911 54635604</p>
+
+        <div className="icons">
+          <div className="box">
+         <a href='https://twitter.com/codeloverK' target="_blank"><img src={twitter} alt="twitter icon" /></a>
+          </div>
+          <div className="box">
+           <a href='https://www.linkedin.com/in/kerlis-aguado' target="_blank"> <img src={linke} alt="twitter icon" /></a>
+          </div>
+          <div className="box">
+          <a href='https://www.behance.net/kerlisaguado26' target="_blank"> <img src={behance} alt="twitter icon" /></a>
+          </div>
+          <div className="box">
+          <a href='https://github.com/keralex' target="_blank"><img src={gitlogo} alt="twitter icon" height='62px' width='60px' /></a>         </div>
+
         </div>
+
+      </div>
     </div>
-)
+  </div>
+);
 
 export default Contact;
