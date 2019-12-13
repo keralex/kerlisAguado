f64fb4b0d9c5bf16885556aa06734bdd5e61a569
keralex <kerlis.aguado26@gmail.com>
2019-12-12 22:42:33 -0300

    Updates

---
+++ precache-manifest.5d40546bae143d707ab9a603cae6199f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e058928753607832ddfd775d75b622e",
+    "url": "/kerlisAguado/index.html"
+  },
+  {
+    "revision": "66e15c6a1a7d05b2bcd4",
+    "url": "/kerlisAguado/static/css/main.5266f7e4.chunk.css"
+  },
+  {
+    "revision": "f1125f2b6e4c82d391e8",
+    "url": "/kerlisAguado/static/js/2.37f0dabb.chunk.js"
+  },
+  {
+    "revision": "66e15c6a1a7d05b2bcd4",
+    "url": "/kerlisAguado/static/js/main.2c31539a.chunk.js"
+  },
+  {
+    "revision": "b480470dc73780411851",
+    "url": "/kerlisAguado/static/js/runtime-main.6f4760c5.js"
+  },
+  {
+    "revision": "76d344f08412543c74ccd1fb2493d607",
+    "url": "/kerlisAguado/static/media/github-logo.76d344f0.svg"
+  },
+  {
+    "revision": "b67b1c3c316804a15f28f9e7817de310",
+    "url": "/kerlisAguado/static/media/imagen.b67b1c3c.jpg"
+  },
+  {
+    "revision": "054081e9245d36b5fbdd24fc36c26d04",
+    "url": "/kerlisAguado/static/media/myself.054081e9.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.89577c8851c47f659dca2daf7bdc19dc.js"
+  "/kerlisAguado/precache-manifest.5d40546bae143d707ab9a603cae6199f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/kerlisAguado/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
