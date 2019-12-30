f8b8b305aecf99a6d8fe5fd87967028725529c71
keralex <kerlis.aguado26@gmail.com>
2019-12-30 00:16:00 -0300

    history text

--- src/components/history-component/history.component.jsx
+++ src/components/history-component/history.component.jsx
@@ -1,23 +1,23 @@
 import React, { Component } from "react";
 import "./history.styles.scss";
-import Profile from '../../assets/imagen.jpg';
+import Profile from "../../assets/imagen.jpg";
 const History = () => (
   <div className="history">
     <div className="left-box">
       <h1>MY HISTORY</h1>
       <p>
-        Hello, my name is Kerlis Aguado and I have been a computer engineering
-        student for 4 years.I have a diploma in software development from the
-        Metroplitana University of Caracas, Venezuela. I have also have done
-        courses in graphic techniques and web development at the CDD in
-        Caracas.I am very passionate about web development and web design, both
+        Hello, my name is Kerlis Aguado. I am a systems engineering student and
+        I am in my last year. Also I have a diploma in software development from the
+        UNIMET, Venezuela. I also have done
+        courses about graphic techniques and web development at the CDD in
+        Caracas. I am very passionate a bout web development and web design, both
         frontend and backend, and I am working to continue improving my skills
         every day.
       </p>
       <h2>WORKING TO BE BETTER</h2>
     </div>
-    <div className='right-box'>
-        <img src={Profile} alt="Kerlis Aguado"/>
+    <div className="right-box">
+      <img src={Profile} alt="Kerlis Aguado" />
     </div>
   </div>
 );
